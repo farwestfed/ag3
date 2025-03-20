d42d704942f6a63f4e547d98c43fbc007f7b626d
Brian Hensarling <81275646+bhensarl@users.noreply.github.com>
2025-03-20 08:50:35 -0400

    Add Army and ASA(IE&E) header with styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import WeatherDamageDashboard from './components/WeatherDamageDashboard';
 function App() {
   return (
     <div className="App">
+      <div className="army-header">
+        <img src="/army_header_banner.png" alt="U.S. Army" className="army-logo" />
+      </div>
+      <div className="yellow-bar"></div>
+      <div className="asa-header">
+        <div className="asa-logo-container">
+          <img src="/asa_ie_logo.png" alt="ASA(IE&E)" className="asa-logo" />
+          <span className="asa-text">ASA(IE&E)</span>
+        </div>
+      </div>
       <WeatherDamageDashboard />
     </div>
   );
